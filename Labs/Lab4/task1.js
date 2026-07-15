f523061c223361da3ec5b8223fd29abb07589032
malakreda-905 <malakreda905@gmail.com>
2026-07-15 13:10:48 +0300

    check palindrom using js

---
+++ Labs/Lab4/task1.js
@@ -0,0 +1,25 @@
+function isPalindrome(str) {
+    var s = str.split("").reverse().join(""); 
+    return str === s; 
+}
+
+function printResult(str){
+}
+
+var str = prompt("Enter a string: ");
+
+var check = confirm("Do you want to consider case sensitivity?");
+
+if(!check){
+    str1 = str.toLowerCase();
+    var result = isPalindrome(str1);
+    console.log("The string " + str + " is " + (result ? "" : "not ") + "a palindrome.");
+    printResult(str);
+}
+
+else{
+    var result = isPalindrome(str);
+    console.log("The string " + str + " is " + (result ? "" : "not ") + "a palindrome.");
+    printResult(str);
+}
+
