16b65bbf2b877df026202921ac7ba45f43b5ecb2
malakreda-905 <malakreda905@gmail.com>
2026-07-21 05:43:18 +0300

    build registeration form

---
+++ Labs/Lab6/task.js
@@ -0,0 +1,6 @@
+var name = document.getElementsByName("name"); 
+var title = document.getElementsByName("job_title"); 
+var email = document.getElementsByName("email"); 
+var mobile = document.getElementsByName("mobile"); 
+var addr = document.getElementsByName("address"); 
+
