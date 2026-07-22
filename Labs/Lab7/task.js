3bae5b7bb9b03fb955f0a4b886f207023074817c
malakreda-905 <malakreda905@gmail.com>
2026-07-22 12:08:01 +0300

    Build next button

--- Labs/Lab7/task.js
+++ Labs/Lab7/task.js
@@ -1,7 +1,7 @@
 function index(){
     for(var i = 0; i < document.images.length; i++){
         if(document.images[i].style.display !== "none"){
-            return i; 
+            return Number(i); 
         }
     }
 }
@@ -9,17 +9,28 @@ function index(){
 document.images[0].style.display = "block"; 
 
 document.querySelector("[value='Slideshow']").addEventListener("click",
-    function (){
+    function(){
+        var idx = index(); 
+        document.images[idx].style.display = "none"; 
         for(let i = 0; i < document.images.length; i++){
             setTimeout(function() 
                 {
                     if(i > 0){
                         document.images[i-1].style.display = "none";
                     }
-                    document.images[i].style.display = "block"
+                    document.images[i].style.display = "block";
                 }, 
             i * 2000); 
         }
     }
 ); 
 
+document.querySelector("[value='Next']").addEventListener("click",
+    function(){
+        var i = index(); 
+        if(i !== document.images.length-1){
+            document.images[i].style.display = "none";
+            document.images[i+1].style.display = "block";
+        }
+    }
+);
