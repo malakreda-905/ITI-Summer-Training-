e352a33474ba40b709de6d6083cd475c97273ba9
malakreda-905 <malakreda905@gmail.com>
2026-07-22 13:34:00 +0300

    Finalize task

--- Labs/Lab7/task.js
+++ Labs/Lab7/task.js
@@ -7,26 +7,47 @@ function index(){
 }
 
 document.images[0].style.display = "block"; 
+var id; 
+var slideshow = false; 
 
 document.querySelector("[value='Slideshow']").addEventListener("click",
     function(){
+        clearInterval(id);
+        slideshow = true; 
         var idx = index(); 
         document.images[idx].style.display = "none"; 
-        for(let i = 0; i < document.images.length; i++){
-            setTimeout(function() 
-                {
-                    if(i > 0){
-                        document.images[i-1].style.display = "none";
-                    }
-                    document.images[i].style.display = "block";
-                }, 
-            i * 2000); 
-        }
+        var i = 0; 
+        document.images[i].style.display = "block";
+        i+=1; 
+        id = setInterval(function() 
+            {
+                if(i === document.images.length){
+                    clearInterval(id); 
+                    slideshow = false; 
+                    return; 
+                }
+                document.images[i-1].style.display = "none"; 
+                document.images[i].style.display = "block";
+                i += 1; 
+            }, 
+        500); 
     }
 ); 
 
+document.querySelector("[value='Stop']").addEventListener("click",
+    function(){
+        var i = index(); 
+        if(slideshow){
+            clearInterval(id);
+            slideshow = false; 
+        }
+    }
+);
+
 document.querySelector("[value='Next']").addEventListener("click",
     function(){
+        clearInterval(id);
+        slideshow = false; 
         var i = index(); 
         if(i !== document.images.length-1){
             document.images[i].style.display = "none";
@@ -37,6 +58,8 @@ document.querySelector("[value='Next']").addEventListener("click",
 
 document.querySelector("[value='Previous']").addEventListener("click",
     function(){
+        clearInterval(id);
+        slideshow = false; 
         var i = index(); 
         if(i !== 0){
             document.images[i].style.display = "none";
@@ -44,4 +67,3 @@ document.querySelector("[value='Previous']").addEventListener("click",
         }
     }
 );
-
