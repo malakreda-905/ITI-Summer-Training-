fcf83c974379410a4ce8a484a0999e596108a88d
malakreda-905 <malakreda905@gmail.com>
2026-07-26 07:28:39 +0300

    Make the welcome message

--- Labs/Lab8/profile.js
+++ Labs/Lab8/profile.js
@@ -1,10 +1,16 @@
-var gen = getCookie("gender"); 
+var gen = getCookie("gender").split("=")[1]; 
+var colr = getCookie("color").split("=")[1]; 
+var name = getCookie("name").split("=")[1];  
 
-var pic = document.getElementById("profile pic"); 
+var pic = document.getElementById("profile pic");
+var wel = document.getElementById("name");  
 
-if(gen.split("=")[1] === 'male'){
+if(gen === 'male'){
     pic.innerHTML = '<img src = "1.jpg">'; 
 } 
 else{
     pic.innerHTML = '<img src = "2.jpg">';
 }
+
+wel.textContent = name;
+wel.style.color = colr;
