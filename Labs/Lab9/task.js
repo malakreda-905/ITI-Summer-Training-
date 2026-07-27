8e588eb7259cb729c13981b2110e43a183ffef5a
malakreda-905 <malakreda905@gmail.com>
2026-07-27 06:44:34 +0300

    Apply high level API methods on array (every, some, filter, map, and forEach)

---
+++ Labs/Lab9/task.js
@@ -0,0 +1,15 @@
+var fruits = ["apple", "strawberry", "banana", "orange", "mango"]; 
+
+var res1 = fruits.every(fruit => fruit instanceof String); 
+console.log(res1); 
+
+var res2 = fruits.some(fruit => fruit[0] === "a"); 
+console.log(res2); 
+
+var res3 = fruits.filter(fruit => fruit[0] === "b" || fruit[0] === "s"); 
+console.log(res3); 
+
+var res4 = fruits.map(fruit => "I like " + fruit); 
+console.log(res4); 
+
+var res5 = res4.forEach(fruit => {console.log(fruit)}); 
