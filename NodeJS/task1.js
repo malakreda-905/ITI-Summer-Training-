9aa630da49c3efa9e0af78e85105bb5b8fe337b4
malakreda-905 <malakreda905@gmail.com>
2026-08-03 10:26:48 +0300

    Handle request message

--- NodeJS/task1.js
+++ NodeJS/task1.js
@@ -1,30 +1,30 @@
 function add() {
-    var result = 0;
-    for(var i = 0; i < arguments.length; i++) {
+    let result = 0;
+    for(let i = 0; i < arguments.length; i++) {
         result += arguments[i];
     }
     return result;
 }
 
 function sub() {
-    var result = arguments[0];
-    for(var i = 1; i < arguments.length; i++) {
+    let result = arguments[0];
+    for(let i = 1; i < arguments.length; i++) {
         result -= arguments[i];
     }
     return result;
 }
 
 function multp() {
-    var result = 1;
-    for(var i = 0; i < arguments.length; i++) {
+    let result = 1;
+    for(let i = 0; i < arguments.length; i++) {
         result *= arguments[i];
     }
     return result;
 }
 
 function div() {
-    var result = arguments[0];
-    for(var i = 1; i < arguments.length; i++) {
+    let result = arguments[0];
+    for(let i = 1; i < arguments.length; i++) {
         if(arguments[i] === 0) {
             throw new Error("Division by zero is not allowed.");
         }
@@ -34,8 +34,81 @@ function div() {
 }
 
 const http = require('http');
-const fs = require('fs');
+const fs = require("fs");
+
 
 const server = http.createServer((req, res) => {
-    
+    const splits = req.url.split("/").filter(Boolean);
+
+    const operation = splits[0];
+    const values = splits.slice(1).map(Number);
+
+    let result;
+
+    try {
+
+        switch(operation) {
+
+            case "add":
+                result = add(...values);
+                break;
+
+            case "sub":
+                result = sub(...values);
+                break;
+
+            case "multp":
+                result = multp(...values);
+                break;
+
+            case "div":
+                result = div(...values);
+                break;
+
+            default:
+                res.writeHead(404, {
+                    "Content-Type": "application/json"
+                });
+
+                return res.end(JSON.stringify({
+                    message: "Invalid operation"
+                }));
+        }
+
+        const responseObj = {
+            operation,
+            values,
+            result,
+            message: `${operation} of ${values.join(", ")} = ${result}`
+        };
+
+        fs.writeFile(
+            "result.json",
+            JSON.stringify(responseObj, null, 2),
+            (err) => {
+                if(err)
+                    console.log(err);
+            }
+        );
+
+        res.writeHead(200, {
+            "Content-Type": "application/json"
+        });
+
+        res.end(JSON.stringify(responseObj));
+
+    } catch(err) {
+
+        res.writeHead(400, {
+            "Content-Type": "application/json"
+        });
+
+        res.end(JSON.stringify({
+            error: err.message
+        }));
+    }
+});
+
+server.listen(5050, () => {
+    console.log("Server is running on port 5050");
 });
